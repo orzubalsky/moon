7d4ac08ae8b3cf90bcbb51662d564bc00e909cf3
orzubalsky <juviley@gmail.com>
2012-12-31 13:42:01 -0500

    print request to debug

--- moon/apps/moonline/views.py
+++ moon/apps/moonline/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from twilio.twiml import Response
 from django_twilio.decorators import twilio_view
+from pywunderground import request as weather
 from moonline.models import *
 
 
@@ -7,6 +9,8 @@ from moonline.models import *
 def answer(request):
     r = Response()
     r.say('Thanks for calling the moon line.')
+    
+    print request
     return r
     
 
@@ -15,3 +19,7 @@ def read(request):
     r = Response()
     r.sms('Thanks for the SMS message!')
     return r
+    
+    
+def moon_over_location(location="New York, NY"):
+    return weather(settings.WEATHER_KEY_ID, ['astronomy',], location)
