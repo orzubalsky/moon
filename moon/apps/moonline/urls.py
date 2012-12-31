5b5304ada5b6491d3c7832856765a11c854fd2d4
orzubalsky <juviley@gmail.com>
2012-12-31 11:00:59 -0500

    fixed basic views names

--- moon/apps/moonline/urls.py
+++ moon/apps/moonline/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('moonline.views',
-    url(r'^answer$', 'index', name='answer'),
-    url(r'^read$', 'index', name='read'),    
+    url(r'^answer$', 'answer', name='answer'),
+    url(r'^read$', 'read', name='read'),    
 )
