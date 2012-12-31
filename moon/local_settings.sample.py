61df3850a405a8bf4813d9d00d08b25a4631b4c4
orzubalsky <juviley@gmail.com>
2012-12-31 10:57:28 -0500

    updated local settings

--- moon/local_settings.sample.py
+++ moon/local_settings.sample.py
@@ -28,6 +28,26 @@ DATABASES = {
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_URL = 'http://127.0.0.1:8000/media/'
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = ''
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    #os.path.join(PROJECT_DIR, 'static/'),    
+)
+
+SECRET_KEY = ''
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
